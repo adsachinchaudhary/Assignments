69e2b6326f2b8452498d18b340c6a0fc3a42bf4d
Sachin <adsachinchaudhary@gmail.com>
2026-03-12 16:34:42 +0530

    Assignment 5

--- assignments/assignment-5.ts
+++ assignments/assignment-5.ts
@@ -1,30 +1,3 @@
-/* Assignment (Conditional Statements & Loops)
-
-Employee Table
-========================
-S.No    Name            Base Salary         Experience (Years)      Year-End Rating (Out of 5)
-1       AliceJohnson    75000.0             5.1                     4.2
-2       BobSmith        68000.0             3.2                     3.8
-3       CarolDavis      82000.0             7.1                     4.5
-4       DavidBrown      90000.0             10.2                    2.5
-5       EvaGreen        60000.0             2.4                     3.5
-
-Hike
-========================
-Rating          % of base Salary as variable pay        Bonus
-> =4.0              15.0                                1500
->=3 && < 4          10.0                                1200
-< 3.0               3.0                                 300
-
-Extra Perks
-=========================
-Employees with Experience >= 5 Years get extra Reward of 5000;
-
-There are 5 Employee records. Now Calculate the Hike percentage of each Employee and store the
-Values in Map with EmployeeName and HikePercentagevalue and Print them.
-
-Hike = ( Base Salary * variable pay % ) + Bonus + Reward ;
-Hike % = Hike / Base Salary . */
 
 interface Employee {
     name: string;
